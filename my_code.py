ff2c282e39ca3463f4ce369e5fa4b12cfabad3c0
23aartale <23aartale@athenian.org>
2020-09-24 16:42:23 +0000

    finsihed average

--- my_code.py
+++ my_code.py
@@ -4,16 +4,16 @@
 def avg(user_list):
     average = sum(user_list) / len(user_list)
     return average
+        
 
 
 if __name__ == '__main__':
+    list = []
+    num = 0
+    while num != 'done':
+        num = input("Enter a number, if done, type 'done': ")
+        if num == 'done':
+            break
+        list.append(int(num))
 
-    user_list = []
-
-    num1 = int(input("Enter your first number: "))
-    num2 = int(input("Enter your second number: "))
-    num3 = int(input("Enter your third number: "))
-    num4 = int(input("Enter your fourth number: "))
-    num5 = int(input("Enter your last number: "))
-
-    print(avg([num1, num2, num3, num4, num5]))
+    print(avg(list))
